e41a2c63058313f93bb56f83b45a0803db972683
Calvin Hsu <jabbacalvin@gmail.com>
2023-06-28 14:31:48 -0500

    completed routing and MVC

---
+++ models/skill.js
@@ -0,0 +1,19 @@
+const skills = [
+    {id: 125223, skill: 'Feed Dogs', done: true},
+    {id: 127904, skill: 'Learn Express', done: false},
+    {id: 139608, skill: 'Buy Milk', done: false}
+];
+
+module.exports = {
+    getAll,
+    getOne
+};
+
+function getAll() {
+    return skills;
+}
+
+function getOne(id) {
+    id = parseInt(id);
+    return skills.find(skill => skill.id === id);
+}
