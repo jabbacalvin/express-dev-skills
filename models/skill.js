f8c9f9855589df6f47ac221db17299a9b5948e7b
Calvin Hsu <jabbacalvin@gmail.com>
2023-06-29 16:19:53 -0500

    completed part 2 of lab and included bootstrap nav, breadcrumb

--- models/skill.js
+++ models/skill.js
@@ -1,13 +1,33 @@
 const skills = [
-    {id: 125223, skill: 'Feed Dogs', done: true},
-    {id: 127904, skill: 'Learn Express', done: false},
-    {id: 139608, skill: 'Buy Milk', done: false}
+    {id: 125223, name: 'Skill 1'},
+    {id: 127904, name: 'Skill 2'},
+    {id: 139608, name: 'Skill 3'}
 ];
 
 module.exports = {
     getAll,
-    getOne
+    getOne,
+    create,
+    deleteOne,
+    update
 };
+
+function update(id, updatedSkill) {
+    id = parseInt(id);
+    const skill = skills.find(skill => skill.id === id);
+    Object.assign(skill, updatedSkill);
+}
+
+function deleteOne(id) {
+    id = parseInt(id);
+    const idx = skills.findIndex(skill => skill.id === id);
+    skills.splice(idx, 1);
+}
+
+function create(skill) {
+    skill.id = Date.now() % 1000000;
+    skills.push(skill);
+}
 
 function getAll() {
     return skills;
